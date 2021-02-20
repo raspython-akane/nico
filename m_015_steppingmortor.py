c759837c9883a7af9015c1d6d1654de6e2b02e2d
gaadesuo <peppu4@gmail.com>
2021-02-21 08:37:12 +0900

    add_test

--- m_015_steppingmortor.py
+++ m_015_steppingmortor.py
@@ -27,7 +27,7 @@ mat_d = 21
 
 # 7seg_ledの点灯パターン
 num_char = [0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7c,
-            0x07, 0x7f, 0x67, 0x00]
+            0x07, 0x7f, 0x67, 0x00, 0x79]
 
 # 1step毎の時間
 # 100Hzとして0.01
@@ -73,6 +73,10 @@ pi_g.i2c_write_byte_data(ht16k33_adr, 0x81, 0x01)
 # INT/LOWの出力はLOW
 pi_g.i2c_write_byte_data(ht16k33_adr, 0xa1, 0x00)
 
+
+"""
+関数定義
+"""
 
 def matrix_sw():
     """
@@ -137,7 +141,7 @@ def zero_padding(n):
     0埋めしたものを桁毎にリストへ入れる
     そのリストをdisplay_charに渡す
     @param n:duty比
-    @type n:int
+    @return: 0詰めしたリストを返す
     """
     # 右寄せ4桁0埋め(str)
     zero_p = str(n).zfill(4)
@@ -146,8 +150,7 @@ def zero_padding(n):
     l = [int(n) for n in list(zero_p)]
     # print("ゼロパディング後: {}".format(l))
 
-    # 7segLEDに表示
-    display_char(l)
+    return l
 
 
 
@@ -155,7 +158,6 @@ def display_char(l):
     """
     与えられた数字を7segLEDで表示する
     @param l: 表示する数字のlist
-    @type l: l
     """
     # リストの値を逆順化
     l.reverse()
@@ -204,7 +206,7 @@ def rotation():
         # 出力の値を渡すのに該当桁が一桁目になるようにビットシフト
         out = (out_val[r_count] & out_pin[i]) >> (3 - i)
         pi_g.write(pin_l[i], out)
-        # print("GPIO_{}の出力 , {}".format(pin_l[i], out))
+        print("GPIO_{}の出力 , {}".format(pin_l[i], out))
 
     # モーター出力が終わったのでflagを0に
     motor_flag = 0
@@ -214,7 +216,9 @@ def input_angle():
     """
     入力角度の変更
     入力モード中は入力桁以外の7segLEDを点滅させる
-    @return: 
+    入力角度は入力桁の値を8と9のスイッチでカウントアップダウン
+    入力桁の移動は10、11のスイッチで左右に変える
+    7番スイッチを押されたら入力終了
     """
     print("入力開始")
     # 入力角度の変数
@@ -235,7 +239,7 @@ def input_angle():
         # 入力時は0.1秒ごと入力角度の入力桁数以外の桁を点滅させる
         # 入力パターン
         if flash_flag == 0:
-            print("入力角度は {}".format(char_l))
+            # print("入力角度は {}".format(char_l))
             display_char(char_l)
             flash_flag = 1
         # 消灯パターン
@@ -265,20 +269,159 @@ def input_angle():
             # 9を超えたら0に戻す
             if inp_angle_n[3 - digits_num] > 9:
                 inp_angle_n[3 - digits_num] = 0
+            print("入力変更後の入力角のリスト{}".format(inp_angle_n))
         # 9番スイッチが押されたら入力角度の入力桁のカウントダウン
         elif 9 in inp_l:
             inp_angle_n[3 - digits_num] -= 1
             # 0より小さくなったら9にする
             if inp_angle_n[3 - digits_num] < 0:
                 inp_angle_n[3 - digits_num] = 9
-
+            print("入力変更後の入力角のリスト{}".format(inp_angle_n))
+        # 10番スイッチが押されたら入力桁を左へずらす
+        elif 10 in inp_l:
+            digits_num += 1
+            # 入力桁が3より大きくなったら0へ戻す
+            if digits_num > 3:
+                digits_num = 0
+            print("入力桁変更後の値 {}".format(digits_num))
+        # 11番スイッチが押されたら入力桁を右へずらす
+        elif 11 in inp_l:
+            digits_num -= 1
+            # 入力桁が0より小さくなったら3にする
+            if digits_num < 0:
+                digits_num = 3
+            print("入力桁変更後の値 {}".format(digits_num))
 
         # 7番スイッチが押されたら入力終了
         if 7 in inp_l:
-            break
+                # 角度の値のチェック
+                if num_check(inp_angle_n) == 0:
+                    inp_angle_n = [0, 0, 0, 0]
+
+                print("入力終了")
+                break
 
         sleep(0.3)
 
+
+def num_check(l):
+    """
+    入力角度のチェック720より大きい場合はエラーを返す
+    @param l:
+    @return: 正常時はTrue,異常時はFaleを返す
+    """
+    print("入力角度のチェック")
+    # 入力角度のチェック。720度より大きい場合はEを1秒間表示して入力角度に0を代入
+    check = 0
+    for i, j in enumerate(l):
+        # リストのインデックス0から順に1000,100,10,1倍したものを加算
+        # print(1000 / (10 ** i))
+        check += j * (1000 / (10 ** i))
+    if check > 7200:
+        print("入力エラー")
+        # EEEEの表示
+        display_char([11, 11, 11, 11])
+        sleep(3)
+
+        return False
+
+    else:
+        return True
+
+
+def step_cal(l=None):
+    """
+    入力角度からstep数を計算する。
+    step数は四捨五入して整数にする。
+    """
+    angle = 0
+
+    # 関数に値を渡されていないときはlにはinp_angle_nが入る
+    if l is None:
+        l = inp_angle_n
+
+    # print("入力角度のリストの値 {}".format(inp_angle_n))
+    # リストの値を数値に直して入力角を整数にする
+    for i, j in enumerate(l):
+        # リストのインデックス0から順に1000,100,10,1倍したものを加算
+        # print(1000 / (10 ** i))
+        angle += j * (1000 / (10 ** i))
+    # print("入力角度の整数 {}".format(angle))
+    # 四捨五入(roundは氏ね)
+    step =  int((((angle * 10) / step_angle) + 5) / 10)
+    # print("入力角度への移動step数 {}".format(step))
+
+    move_to_angle(step)
+
+
+def move_to_angle(step):
+    """
+    現在のstep数から入力角度のstep数への移動指示
+    @param step: 入力角度のステップ数
+    @return:
+    """
+    while True:
+        # 現在角度の表示
+        l = zero_padding(step_count * step_angle)
+        display_char(l)
+
+        # 移動先step数と現在のstep数が同じなら何もしないで抜ける
+        if step == step_count:
+            break
+        # CW方向
+        elif step > step_count:
+            print("CWへ移動残り {}step".format(step - step_count))
+            count_control(1)
+        # CCW方向
+        elif step < step_count:
+            print("CCWへ移動残り {}step".format(step_count - step))
+            count_control(-1)
+
+        # 回転指示
+        rotation()
+
+        sleep(sleep_time)
+
+
+def read_txt():
+    """
+    step.txtの中の角度を数値化してstep数計算関数に渡す
+    """
+    angle_list = []
+    try:
+        # テキストファイルから読み込み
+        with open("step.txt", "r", encoding="UTF-8") as inp_txt:
+            # 行ごとに数値に変換
+            for txt in inp_txt:
+                    # 数字の場合は数値化してリストへ格納
+                    try:
+                        # print("数値化する値 {}".format(txt))
+                        # 入力は小数点第一位まであるので
+                        # 10倍して整数にする
+                        angle = [int(float(i) * 10) for i in txt.split()]
+                        angle_list.append(angle[0])
+
+                        # print("テキストから読みこんだ値のリスト {}".format(angle_list))
+                    # 数字じゃないときはエラー表示して抜ける
+                    # 入力されたリストは空にする
+                    except ValueError:
+                        print("step.txtエラー")
+                        display_char([11, 11, 11, 11])
+                        sleep(3)
+                        angle_list = []
+                        break
+
+            # 数値化したリストの中身を順に0埋め関数に渡してリスト化
+            for i in angle_list:
+                l = zero_padding(i)
+                print("step.txtの{}を桁ごとにリスト化 {}".format(i, l))
+
+                # リスト化したものをstep計算関数へ渡す。
+                step_cal(l)
+                sleep(1)
+
+    except FileNotFoundError:
+        print("設定ファイルが見つかりません")
 
 
 def jog(flag):
@@ -289,13 +432,15 @@ def jog(flag):
     橙  : 1づつカウントダウン
     その後モーターの回転制御関数を呼ぶ。
     @param flag: 回転方向の値(int)
-    @return:
     """
+    # CW方向
     if step_count < 400 and flag == 1:
         count_control(flag)
+    # CCW方向
     elif step_count > 0 and flag == -1:
         count_control(flag)
 
+    # 回転指示
     rotation()
 
 
@@ -307,8 +452,9 @@ def control():
     """
 
     while True:
-        # ステップ数にステップ角の10倍の18
-        zero_padding(step_count * step_angle)
+        # 現在角度の表示
+        l = zero_padding(step_count * step_angle)
+        display_char(l)
 
         # キーマトリクスのチェック
         inp_l = matrix_sw()
@@ -316,10 +462,16 @@ def control():
         # スイッチ3が押されたら角度入力
         if 3 in inp_l:
             input_angle()
+        # スイッチ1が押されたら入力角からstep数を計算し移動
+        elif 1 in inp_l:
+            step_cal()
+        # スイッチ5が押されたらテキストの入力角へ順次移動
+        elif 5 in inp_l:
+            read_txt()
         # スイッチ2が押されたらCW方向にjog運転
         elif 2 in inp_l:
             jog(1)
-
+        # スイッチ6が押されたらCCW方向にjog運転
         elif 6 in inp_l:
             jog(-1)
 
