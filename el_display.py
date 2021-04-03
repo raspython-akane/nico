07fd126482d3bbf4a1f62b358464dbe714d4b81a
gaadesuo <peppu4@gmail.com>
2021-04-04 07:55:48 +0900

    add_test

--- el_display.py
+++ el_display.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Filename: SO1602A 
+# Filename: SO1602A
 __author__ = "raspython"
 __date__ = '2021/04/04 06:43'
 
@@ -20,6 +20,10 @@ class SO1602A():
         # 1はbus番号、0x3cはスレーブアドレス。
         self.so1602_adr = self.pi_g.i2c_open(1, 0x3c)
 
+        self.clear_display()
+        self.return_home()
+        self.display_on()
+
 
     def clear_display(self):
         """
@@ -33,6 +37,24 @@ class SO1602A():
         Return Home
         """
         self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, 0x02)
+
+
+    def display_on(self):
+        """
+        表示設定
+        パラメータ 0000 01DCB
+
+        D = 1 ディスプレイON
+        D = 0 ディスプレイOFF
+
+        C = 1 カーソルON
+        C = 0 カーソルOFF
+
+        B = 1 カーソル位置を点滅させる
+        B = 0 カーソル位置を点滅させない
+
+        """
+        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, 0x0f)
 
 
 
