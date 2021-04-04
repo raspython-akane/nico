be2e1ba22cbd734fe86cfa88952a38f9e69bdcd3
gaadesuo <peppu4@gmail.com>
2021-04-04 13:39:19 +0900

    add_test

--- el_display.py
+++ el_display.py
@@ -5,14 +5,13 @@ __author__ = "raspython"
 __date__ = '2021/04/04 06:43'
 
 import pigpio as pi
+from time import sleep
 
 
-class SO1602A():
+class SO1602A:
 
     def __init__(self):
-        """
-        初期化
-        """
+
         # pigpioの用意
         self.pi_g = pi.pi()
 
@@ -22,51 +21,122 @@ class SO1602A():
 
         self.clear_display()
         self.return_home()
-        self.display_on()
+        self.display_method()
+        self.display_set()
+        self.print_line()
+
+
+    """
+    setting
+    """
 
 
     def clear_display(self):
         """
-        Clear Display
+        ディスプレイ表示をクリアする
         """
         self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, 0x01)
+        # print("ディスプレイの表示をクリア")
 
 
     def return_home(self):
         """
-        Return Home
+        カーソルを左上に戻す
         """
         self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, 0x02)
+        # print("カーソルを初期値へ")
 
 
-    def display_on(self):
+    def display_method(self, reverse=False, one_character=False):
         """
-        表示設定
-        パラメータ 0000 01DCB
-
-        D = 1 ディスプレイON
-        D = 0 ディスプレイOFF
-
-        C = 1 カーソルON
-        C = 0 カーソルOFF
-
-        B = 1 カーソル位置を点滅させる
-        B = 0 カーソル位置を点滅させない
-
+        カーソルの移動方向を設定 Trueならカーソルから逆方向へ表示
+        one_charcterはなんやこれ？
+        @param reverse: カーソルの移動方向 Trueで右 Falseで左へシフト
+        @param one_character:よくわからん！さわるな！
         """
-        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, 0x0f)
+        self.val = 0x06
+        if reverse:
+            self.val -= 0x02
+        if one_character:
+            self.val += 0x01
+
+        # 設定を流す
+        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, self.val)
 
 
-    def char_display(self, s):
+    def display_set(self, display=True, cursor=True,  blink=True):
+        """
+        ディスプレイの表示設定
+        @param display: ディスプレイの表示設定 default = True
+        @param cursor: カーソルの表示 default = True
+        @param blink:  カーソル位置の点滅 default = True
         """
 
-        @param s: 文字列
+        self.val= 0x08
+        # ディスプレイの表示
+        if display:
+            self.val += 0x01
+        # カーソルの表示
+        if cursor:
+            self.val += 0x02
+        # カーソル位置の点滅表示
+        if blink:
+            self.val += 0x04
+
+        # 設定を流す
+        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, self.val)
+
+
+    def cursor_display_shift(self, right_shift=True, display_shift=False):
+        """
+        カーソルの移動
+        @param r_l:
         @return:
+        """
+        self.val = 0x10
+        if right_shift:
+            self.val += 0x04
+        if display_shift:
+            self.val += 0x08
+
+        # 設定を流す
+        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, self.val)
+
+
+    def print_line(self, tow_line=True, double_high=False):
+        """
+        表示ラインと表示の高さ設定
+        double_highにする場合は表示は2LINEにする
+        @param towline: 文字を表示するライン default = 2LINE
+        @param double_high: 文字のフォントの高さ default = 1LINE
+        """
+        self.val = 0x20
+        # 表示ラインの設定
+        if tow_line:
+            self.val += 0x08
+        if double_high:
+            self.val += 0x04
+
+        # 設定を流す
+        self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x00, self.val)
+
+
+    """
+    文字の書き込み
+    """
+
+
+    def print(self, s, time=0):
+        """
+        ディスプレイに表示する
+        @param s: 表示する文字列
+        @param time: 文字毎の表示ディレイ default = 0
         """
         l = s.encode("shift_jis")
 
         for i in l:
             self.pi_g.i2c_write_byte_data(self.so1602_adr, 0x40, i)
+            sleep(time)
 
 
 
--- test2.py
+++ test2.py
@@ -8,4 +8,31 @@ __date__ = '2021/04/04 08:03'
 import el_display
 
 d = el_display.SO1602A()
-d.char_display("1234ABCDEabcdef##$$%%&$#ｱｲｳｴｵ")
+
+while True:
+    w = int(input())
+    if w == 0:
+        break
+    if w == 1:
+        d.clear_display()
+        d.return_home()
+    if w == 2:
+        d.print("1234ABCDEabcdef##$$%%&$#ｱｲｳｴｵ", time=0.1)
+    if w == 3:
+        d.print_line(double_high=True)
+    if w == 4:
+        d.print_line(double_high=False)
+    if w == 5:
+        d.display_method(reverse=False)
+    if w == 6:
+        d.display_method(reverse=True)
+    if w == 7:
+        d.display_method(one_character=True)
+    if w == 8:
+        d.display_method(one_character=False)
+    if w == 9:
+        d.cursor_display_shift()
+    if w == 10:
+        d.cursor_display_shift(right_shift=False)
+    if w == 11:
+        d.cursor_display_shift(display_shift=True)
