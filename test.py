4b34ee7e72c80ed470c2437fc905994eef9ad1da
gaadesuo <peppu4@gmail.com>
2021-05-14 16:49:03 +0900

    add_test

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 
+rou = lambda x: (x * 2 + 1) // 2
 
-print("ABC123#$%ｱｲｳ".encode("shift_jis"))
-print(b"ABCDEF123#$%")
+ans = rou(1.45)
+print(ans)
 
-print(hex(ord("A")))
-
-# インポート
-import el_display
-
-val = 0
-val |= 0b000_00100
-print(bin(val))
+def rou(x):
+    ans = (x * 2 + 1) // 2
+    return ans
