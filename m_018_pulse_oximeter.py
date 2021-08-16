7e505494337888dd49725c84b856377f1cb6025b
gaadesuo <peppu4@gmail.com>
2021-08-16 20:14:56 +0900

    add_test

--- m_018_pulse_oximeter.py
+++ m_018_pulse_oximeter.py
@@ -4,10 +4,120 @@
 __author__ = "raspython"
 __date__ = '2021/08/15 20:09'
 
+import pigpio as pi
+from time import sleep
+import el_display
+
+
+# pinの定義
+int_pin = 26
+
+# データの取得回数
+sampling = 1000
+
+# カウンタ変数
+count_var = 0
+
+# GPIOの初期設定
+pi_g = pi.pi()
+# 入力設定
+pi_g.set_mode(int_pin, pi.INPUT)
+
+# max30102のi2C設定
+# 1はbus番号、0x57はスレーブアドレス
+max30102_adr = pi_g.i2c_open(1, 0x57)
+
+def reset():
+    """
+    データの各レジスタをパワーオン状態にリセットする
+    レジスタアドレス0x09に0x40を与える
+    """
+    pi_g.i2c_write_byte_data(max30102_adr, 0x09, 0x40)
+
+def read_date(pin, status, tick):
+    """
+    FIFOデータレジスタを読みこむ。
+    """
+    global count_var
+
+    red = None
+    ir = None
+
+    # New FIFO Data Readyを読み込んで割り込みをクリアする
+    reg_date1 = pi_g.i2c_read_byte_data(max30102_adr, 0x00)
+    # Internal Temperature Ready Flagを読み込んでクリアする
+    reg_date2 = pi_g.i2c_read_byte_data(max30102_adr, 0x01)
+
+
+    read_d = pi_g.i2c_read_i2c_block_data(max30102_adr, 0x07, 6)
+    # print("FIFI date regの値: {}".format(read_d))
+    # 1byteデータをシフトし、3byte(24bit)のデータにして下位18bitでマスクする
+    # 18bitなのはpulse widthが411μs samples per second が100の値の為
+    red = (read_d[1][0] << 16 | read_d[1][1] << 8 | read_d[1][2]) & 0b0011_1111_1111_1111_1111
+    ir = (read_d[1][3] << 16 | read_d[1][4] << 8 | read_d[1][5]) & 0b0011_1111_1111_1111_1111
+
+    print(red, ir)
+    count_var += 1
+
+    return read_d
+
+def shutdown():
+    """
+    シャットダウンコントロール処理
+    """
+    pi_g.i2c_write_byte_data(max30102_adr, 0x09, 0x80)
 
 def main():
-    pass
+    # データレジスタの初期化
+    reset()
+    sleep(1)
 
+    # レジスタの設定
+    # 割り込み制御の設定
+    pi_g.i2c_write_byte_data(max30102_adr, 0x02, 0xc0)
+    pi_g.i2c_write_byte_data(max30102_adr, 0x03, 0x00)
+
+    # FIFOセッティング
+    # FIFO Write Pointer
+    pi_g.i2c_write_byte_data(max30102_adr, 0x04, 0x00)
+    # FIFO Overflow Counter
+    pi_g.i2c_write_byte_data(max30102_adr, 0x05, 0x00)
+    # FIFO Read Pointer
+    pi_g.i2c_write_byte_data(max30102_adr, 0x06, 0x00)
+    # FIFO Configuration
+    pi_g.i2c_write_byte_data(max30102_adr, 0x08, 0x4f)
+    # mode control
+    pi_g.i2c_write_byte_data(max30102_adr, 0x09, 0x03)
+    # SpO2 Configuration
+    pi_g.i2c_write_byte_data(max30102_adr, 0x0a, 0x27)
+    # LED1の電流設定
+    pi_g.i2c_write_byte_data(max30102_adr, 0x0c, 0x24)
+    # LED2の電流設定
+    pi_g.i2c_write_byte_data(max30102_adr, 0x0d, 0x24)
+    # pilotLEDの電流設定
+    pi_g.i2c_write_byte_data(max30102_adr, 0x10, 0x7f)
+    # Power Ready Flagを読みこんで初期化
+    reg_date1 = pi_g.i2c_read_byte_data(max30102_adr, 0x00)
+
+    try:
+        """
+        while count_var < sampling:
+            # 割り込み信号が立下り時に読み込み関数を呼ぶ
+            cb = pi_g.callback(int_pin, pi.FALLING_EDGE, read_date)
+        """
+        for i in range(1000):
+            while pi_g.read(int_pin) == 1:
+                pass
+            read_date(0,0,0)
+
+
+
+    except KeyboardInterrupt:
+        pass
+
+    # cb.cancel()
+    shutdown()
+    pi_g.stop()
 
 if __name__ == '__main__':
     main()
--- max30102-tutorial-raspberrypi-master/max30102.py
+++ max30102-tutorial-raspberrypi-master/max30102.py
@@ -1,33 +1,39 @@
-import smbus
+# -*-coding:utf-8-*-
+
+# this code is currently for python 2.7
+from __future__ import print_function
 from time import sleep
-import datetime
-import pigpio as pi
 
-# pinの定義
-int_pin = 26
+import RPi.GPIO as GPIO
+import smbus
 
-# GPIOの初期設定
-pi_g = pi.pi()
-# 入力設定
-pi_g.set_mode(int_pin, pi.INPUT)
+# i2c address-es
+# not required?
+I2C_WRITE_ADDR = 0xAE
+I2C_READ_ADDR = 0xAF
 
-
+# register address-es
 REG_INTR_STATUS_1 = 0x00
 REG_INTR_STATUS_2 = 0x01
+
 REG_INTR_ENABLE_1 = 0x02
 REG_INTR_ENABLE_2 = 0x03
+
 REG_FIFO_WR_PTR = 0x04
 REG_OVF_COUNTER = 0x05
 REG_FIFO_RD_PTR = 0x06
 REG_FIFO_DATA = 0x07
 REG_FIFO_CONFIG = 0x08
+
 REG_MODE_CONFIG = 0x09
 REG_SPO2_CONFIG = 0x0A
 REG_LED1_PA = 0x0C
+
 REG_LED2_PA = 0x0D
 REG_PILOT_PA = 0x10
 REG_MULTI_LED_CTRL1 = 0x11
 REG_MULTI_LED_CTRL2 = 0x12
+
 REG_TEMP_INTR = 0x1F
 REG_TEMP_FRAC = 0x20
 REG_TEMP_CONFIG = 0x21
@@ -35,29 +41,54 @@ REG_PROX_INT_THRESH = 0x30
 REG_REV_ID = 0xFE
 REG_PART_ID = 0xFF
 
+# currently not used
+MAX_BRIGHTNESS = 255
+
+
 class MAX30102():
-    # setup()相当の部分
+    # by default, this assumes that physical pin 7 (GPIO 4) is used as interrupt
+    # by default, this assumes that the device is at 0x57 on channel 1
     def __init__(self, channel=1, address=0x57, gpio_pin=7):
-        # Raspberry Piでi2cを行う各種設定
+        print("Channel: {0}, address: 0x{1:x}".format(channel, address))
         self.address = address
         self.channel = channel
-        # i2cのセットアップ
         self.bus = smbus.SMBus(self.channel)
+        self.interrupt = gpio_pin
+
+        # set gpio mode
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.interrupt, GPIO.IN)
 
         self.reset()
 
-        sleep(1) # wait 1 sec
+        sleep(1)  # wait 1 sec
 
-        # 割り込みレジスタを読み捨て(この心拍センサは割り込みレジスタを読むと割り込み状態をクリアする)
+        # read & clear interrupt register (read 1 byte)
         reg_data = self.bus.read_i2c_block_data(self.address, REG_INTR_STATUS_1, 1)
-
+        # print("[SETUP] reset complete with interrupt register0: {0}".format(reg_data))
         self.setup()
+        # print("[SETUP] setup complete")
+
+    def shutdown(self):
+        """
+        Shutdown the device.
+        """
+        self.bus.write_i2c_block_data(self.address, REG_MODE_CONFIG, [0x80])
 
     def reset(self):
+        """
+        Reset the device, this will clear all settings,
+        so after running this, run setup() again.
+        """
         self.bus.write_i2c_block_data(self.address, REG_MODE_CONFIG, [0x40])
 
     def setup(self, led_mode=0x03):
+        """
+        This will setup the device with the values written in sample Arduino code.
+        """
         # INTR setting
+        # 0xc0 : A_FULL_EN and PPG_RDY_EN = Interrupt will be triggered when
+        # fifo almost full & new fifo data ready
         self.bus.write_i2c_block_data(self.address, REG_INTR_ENABLE_1, [0xc0])
         self.bus.write_i2c_block_data(self.address, REG_INTR_ENABLE_2, [0x00])
 
@@ -68,11 +99,13 @@ class MAX30102():
         # FIFO_RD_PTR[4:0]
         self.bus.write_i2c_block_data(self.address, REG_FIFO_RD_PTR, [0x00])
 
+        # 0b 0100 1111
         # sample avg = 4, fifo rollover = false, fifo almost full = 17
         self.bus.write_i2c_block_data(self.address, REG_FIFO_CONFIG, [0x4f])
 
         # 0x02 for read-only, 0x03 for SpO2 mode, 0x07 multimode LED
         self.bus.write_i2c_block_data(self.address, REG_MODE_CONFIG, [led_mode])
+        # 0b 0010 0111
         # SPO2_ADC range = 4096nA, SPO2 sample rate = 100Hz, LED pulse-width = 411uS
         self.bus.write_i2c_block_data(self.address, REG_SPO2_CONFIG, [0x27])
 
@@ -83,8 +116,15 @@ class MAX30102():
         # choose value fro ~25mA for Pilot LED
         self.bus.write_i2c_block_data(self.address, REG_PILOT_PA, [0x7f])
 
-    # 赤色/IRのLEDのデータを読むのはこれ
+    # this won't validate the arguments!
+    # use when changing the values from default
+    def set_config(self, reg, value):
+        self.bus.write_i2c_block_data(self.address, reg, value)
+
     def read_fifo(self):
+        """
+        This function will read the data register.
+        """
         red_led = None
         ir_led = None
 
@@ -96,29 +136,27 @@ class MAX30102():
         d = self.bus.read_i2c_block_data(self.address, REG_FIFO_DATA, 6)
 
         # mask MSB [23:18]
-        red_led = (d[0]<<16 | d[1] << 8 | d[2]) & 0x03FFFF
-        ir_led = (d[3]<<16 | d[4] << 8 | d[5]) & 0x03FFFF
+        red_led = (d[0] << 16 | d[1] << 8 | d[2]) & 0x03FFFF
+        ir_led = (d[3] << 16 | d[4] << 8 | d[5]) & 0x03FFFF
 
         return red_led, ir_led
 
-def main():
-    m = MAX30102()
-    try:
-        m.setup()
-
-        for i in range(100):
-            while pi_g.read(int_pin) == 1:
+    def read_sequential(self, amount=100):
+        """
+        This function will read the red-led and ir-led `amount` times.
+        This works as blocking function.
+        """
+        red_buf = []
+        ir_buf = []
+        for i in range(amount):
+            while(GPIO.input(self.interrupt) == 1):
+                # wait for interrupt signal, which means the data is available
+                # do nothing here
                 pass
-            red, ir = m.read_fifo()
-            time = datetime.datetime.now()
-            print(time, red, ir)
 
+            red, ir = self.read_fifo()
 
+            red_buf.append(red)
+            ir_buf.append(ir)
 
-    except KeyboardInterrupt:
-
-        pass
-
-
-if __name__ == '__main__':
-    main()
+        return red_buf, ir_buf
