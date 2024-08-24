bd29b225880d33a0beab810acebf394220bc731e
gaadesuo <peppu4@gmail.com>
2024-08-24 18:04:03 +0900

    add_test

--- test2.py
+++ test2.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
+import sys
 
-# Filename: test2 
-__author__ = "raspython"
-__date__ = '2024/07/26 22:02'
-
-
-def main():
-    print(0.1 + 0.1 + 0.1)
-    # 0.30000000000000004
-
-    print(0.1 + 0.1 + 0.1 == 0.3)
-    # False
-
-if __name__ == '__main__':
-    main()
+sys.base_prefix
